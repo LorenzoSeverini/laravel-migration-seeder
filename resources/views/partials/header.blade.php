df473b571c99b2e862d8eb2f6b65461559fafea7
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-29 17:59:59 +0200

    add nav link n logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,12 @@
 <header class="p-3">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
-    <span>Some links: </span>
-    @foreach ($links as $link)
-        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
-    @endforeach
+    <nav class="navbar">
+        <a class="navbar-brand" target="_blank" href="https://www.nexi.it/it"><img src="{{ Vite::asset('/resources/img/logo.png') }}" class="img-fluid" style="width: 100px; height: 100px;" alt="Logo"></a>
+        <ul class="nav-links">
+            @foreach ($links as $link)
+                <li class="nav-link">
+                    <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </nav>
 </header>
