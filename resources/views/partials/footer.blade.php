32e6615a9b288f81b9ca30a3c78613ad9777b479
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-29 17:59:40 +0200

    add footer nav links and social icons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,22 @@
-<footer class="p-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</footer>
+<footer class="p-3">
+    <nav class="navbar d-flex justify-content-center">
+        <ul class="nav-links d-flex gap-3">
+            @foreach ($links as $link)
+                <li class="nav-link">
+                    <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </nav>
+    <div class="text-center">
+        <div class="d-flex justify-content-center gap-3 p-3">
+            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
+        <div class="text-center">
+            <p>&copy; {{ date('Y') }} Movies App. All rights reserved.</p>
+        </div>
+    </div>
+
+</footer>
