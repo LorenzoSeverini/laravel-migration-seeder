de0ca8e3948e105c9ba815da87df843751a63750
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-01 15:47:52 +0200

    fix:database n new logic for data from seeder.php

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,70 +4,32 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Train;
-use Carbon\Carbon;
 
 class TrainsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
+        $trains = config('seeder.trains');
+        foreach ($trains as $trainData) {
 
-        $currentDate = Carbon::now()->toDateString();
+            $existingTrain = Train::where('codice_treno', $trainData['codice_treno'])->first();
 
-        Train::firstOrCreate([
-            'azienda' => 'Azienda 1',
-            'stazione_partenza' => 'Stazione A',
-            'stazione_arrivo' => 'Stazione B',
-            'orario_partenza' => '08:00:00',
-            'orario_arrivo' => '10:00:00',
-            'codice_treno' => 'ABC123',
-            'numero_carrozze' => 5,
-            'in_orario' => true,
-            'cancellato' => false,
-            'data' => $currentDate,
-        ]);
-
-        Train::firstOrCreate([
-            'azienda' => 'Azienda 2',
-            'stazione_partenza' => 'Stazione C',
-            'stazione_arrivo' => 'Stazione D',
-            'orario_partenza' => '12:00:00',
-            'orario_arrivo' => '14:00:00',
-            'codice_treno' => 'DEF456',
-            'numero_carrozze' => 3,
-            'in_orario' => true,
-            'cancellato' => false,
-            'data' => $currentDate,
-        ]);
-
-        Train::firstOrCreate([
-            'azienda' => 'Azienda 3',
-            'stazione_partenza' => 'Stazione E',
-            'stazione_arrivo' => 'Stazione F',
-            'orario_partenza' => '19:00:00',
-            'orario_arrivo' => '20:00:00',
-            'codice_treno' => 'GHI789',
-            'numero_carrozze' => 7,
-            'in_orario' => true,
-            'cancellato' => false,
-            'data' => $currentDate,
-        ]);
-
-        Train::firstOrCreate([
-            'azienda' => 'Azienda 4',
-            'stazione_partenza' => 'Stazione G',
-            'stazione_arrivo' => 'Stazione H',
-            'orario_partenza' => '22:00:00',
-            'orario_arrivo' => '23:00:00',
-            'codice_treno' => 'JKL012',
-            'numero_carrozze' => 2,
-            'in_orario' => true,
-            'cancellato' => false,
-            'data' => $currentDate,
-        ]);
+            if ($existingTrain) {
+                continue;
+            } else {
+                $train = new Train();
+                $train->azienda = $trainData['azienda'];
+                $train->stazione_partenza = $trainData['stazione_partenza'];
+                $train->stazione_arrivo = $trainData['stazione_arrivo'];
+                $train->orario_partenza = $trainData['orario_partenza'];
+                $train->orario_arrivo = $trainData['orario_arrivo'];
+                $train->codice_treno = $trainData['codice_treno'];
+                $train->numero_carrozze = $trainData['numero_carrozze'];
+                $train->in_orario = $trainData['in_orario'];
+                $train->cancellato = $trainData['cancellato'];
+                $train->data = $trainData['data'];
+                $train->save();
+            }
+        }
     }
-};
+}
